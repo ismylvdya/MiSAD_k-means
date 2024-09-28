83d575b321bf7757543bc2c31fee4812ac7b9fe9
ismylvdya <ismylvdya@gmail.com>
2024-09-29 02:16:33 +0300

    нормализация датасета добавлена сюда, переделано в класс, targets геттерится как нормальный массив, есть возсожность геттернуть features уже нормализованным

--- datasets/wine.py
+++ datasets/wine.py
@@ -1,18 +1,66 @@
 from ucimlrepo import fetch_ucirepo
+import numpy as np
 
-# fetch dataset
-wine_dataset = fetch_ucirepo(id=109)
+def normalized(data):
+    '''Функция для нормализации данных
+    принимает на вход двумерный numpy массив
+    возвращает нормализованный массив (в нем теперь значения из [0,1])'''
 
-def features():
-    '''Return features of wine dataset as numpy array (178 объектов (строк) и 13 признаков (столбцов))'''
-    return wine_dataset.data.features.to_numpy()  # data импортируется как pandas dataframes поэтому конвертируем в numpy массивы
+    data_new = np.copy(data)
+    for j in range(len(data[0])): # перебираем все строки датасета
+        min_val = np.min(data[:, j])
+        max_val = np.max(data[:, j])
+        for i in range(len(data)):
+            data_new[i][j] = (data[i][j] - min_val) / (max_val - min_val)
+    return data_new
 
-def targets():
-    '''Return targets of wine dataset as numpy array'''
-    return wine_dataset.data.targets.to_numpy()  # data импортируется как pandas dataframes поэтому конвертируем в numpy массивы
+class WineDataset:
+    def __init__(self, dataset_id=109):
+        # Загружаем набор данных при инициализации класса
+        self.wine_dataset = fetch_ucirepo(id=dataset_id)
 
-def print_metadata():
-    print(wine_dataset.metadata)
+        self.k = 3  # Количество кластеров
 
-def print_variable_inf():
-    print(wine_dataset.variables)
+        # Устанавливаем названия признаков
+        self.axis_names = {
+            0: 'Alcohol',
+            1: 'Malic acid',
+            2: 'Ash',
+            3: 'Alcalinity of ash',
+            4: 'Magnesium',
+            5: 'Total phenols',
+            6: 'Flavanoids',
+            7: 'Nonflavanoid phenols',
+            8: 'Proanthocyanins',
+            9: 'Color intensity',
+            10: 'Hue',
+            11: 'OD280/OD315 of diluted wines',
+            12: 'Proline'
+        }
+
+    def get_features(self):
+        '''Возвращает весь датасет как numpy массив'''
+        return self.wine_dataset.data.features.to_numpy()
+
+    def get_normalized_features(self):
+        '''Возвращает весь датасет НОРМАЛИЗОВАННЫЙ ДО [0,1] как numpy массив'''
+        return normalized(self.wine_dataset.data.features.to_numpy())
+
+    def get_axis_name(self, n):
+        '''Возвращает название признака по его номеру'''
+        return self.axis_names[n] if n in [0,12] else None
+
+    def get_targets(self):
+        '''Возвращает эталонную кластеризацию как нормальный numpy массив'''
+        targets = []
+        for el in self.wine_dataset.data.targets.to_numpy():
+            targets.append(*el)
+        return targets
+
+    def print_metadata(self):
+        '''Печатает метаданные набора данных'''
+        print(self.wine_dataset.metadata)
+
+    def print_variable_info(self):
+        '''Печатает информацию о переменных набора данных'''
+        print(self.wine_dataset.variables)
